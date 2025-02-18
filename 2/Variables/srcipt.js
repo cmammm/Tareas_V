55e562658c189421c36b683ed30dc1b75b59fb57
cmammm <166537207+cmammm@users.noreply.github.com>
2025-02-18 11:07:39 -0600

    Add files via upload

---
+++ 2/Variables/srcipt.js
@@ -0,0 +1,8 @@
+//variables
+
+let rosa = "pastel";
+let rbg1 = 255204203;
+
+//document.write(rosa);
+document.getElementById('demo').innerHTML="Un tono del rosa es el rosa:  " + rosa;
+document.getElementById('color').innerHTML="El codigo RBG del rosa pastel es: " + rbg1;
