5f51908d9424f287b957ba5ea0827cb2c777d94f
Cam <2233072987@correo.ler.uam.mx>
2025-03-11 10:58:17 -0600

    cinco

--- 5/script.js
+++ 5/script.js
@@ -1,6 +1,11 @@
 let fl = "Transfusión de magia pura para el corazón Rímel de miel para corregir la tristeza Tattoo mentalpara marcarse la imaginación Tragos de luz para alegrarse la vida";
+let fl2 = fl.toUpperCase();
+let fl3 = fl2.replace("MIEL", "SANGRE");
 
 document.getElementById('frase').innerHTML=fl;
+document.getElementById('frase2').innerHTML=fl2;
+document.getElementById('frase3').innerHTML=fl3;
 
 //console.log(fl.length);
-console.log(fl.at(8));
+//console.log(fl.at(8));
+console.log(fl.toUpperCase());
