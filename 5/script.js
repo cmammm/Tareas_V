68fbaf7982892ef060459520ccc1a459f4dba3f0
Cam <2233072987@correo.ler.uam.mx>
2025-03-11 09:32:58 -0600

    cinco

---
+++ 5/script.js
@@ -0,0 +1,6 @@
+let fl = "Transfusión de magia pura para el corazón Rímel de miel para corregir la tristeza Tattoo mentalpara marcarse la imaginación Tragos de luz para alegrarse la vida";
+
+document.getElementById('frase').innerHTML=fl;
+
+//console.log(fl.length);
+console.log(fl.at(8));
